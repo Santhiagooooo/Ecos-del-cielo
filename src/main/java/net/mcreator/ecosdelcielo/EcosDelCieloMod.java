eb1c537867b72877927ad349459dcafa17494066
martinhub777 <perez@martinAsus>
2024-12-05 22:43:14 -0500

    Agregamos cubo de lechita

--- src/main/java/net/mcreator/ecosdelcielo/EcosDelCieloMod.java
+++ src/main/java/net/mcreator/ecosdelcielo/EcosDelCieloMod.java
@@ -17,6 +17,10 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
+import net.mcreator.ecosdelcielo.init.EcosDelCieloModTabs;
+import net.mcreator.ecosdelcielo.init.EcosDelCieloModItems;
+import net.mcreator.ecosdelcielo.init.EcosDelCieloModBlocks;
+
 import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.BiConsumer;
@@ -36,6 +40,12 @@ public class EcosDelCieloMod {
 		// End of user code block mod constructor
 		MinecraftForge.EVENT_BUS.register(this);
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		EcosDelCieloModBlocks.REGISTRY.register(bus);
+
+		EcosDelCieloModItems.REGISTRY.register(bus);
+
+		EcosDelCieloModTabs.REGISTRY.register(bus);
 
 		// Start of user code block mod init
 		// End of user code block mod init
